2530e8ba774975d484687c8f19f6012e49a3fe39
Daniel Siegl <daniel.siegl@lieberlieber.com>
2025-03-20 18:41:23 +0100

    Update API endpoint to OpenAI, fix prompt and output formatting

--- runGitHubModels.ps1
+++ runGitHubModels.ps1
@@ -8,12 +8,14 @@ if ($PSVersionTable.PSEdition -ne "Core") {
 . .\functions\Get-ApiToken.ps1
 . .\functions\Invoke-ChatCompletion.ps1
 
-$baseUrl = "http://localhost:11434/"  #"https://models.inference.ai.azure.com"
-$apiKey = "" # Get-ApiToken  # Ensure you have set this environment variable
-$model = "llama3:latest" #"4o-mini" #"gpt-4o"  # Specify the model you want to use
+$baseUrl = "https://api.openai.com/v1/"  #"https://models.inference.ai.azure.com"
+$apiKey = Get-ApiToken  # Ensure you have set this environment variable
+$model = "o3-mini" #"4o-mini" #"gpt-4o"  # Specify the model you want to use
 
-$prompt = "What is the {  capital '`" of France?"
+$prompt = "What is the capital of France?"
 
 # Example usage
+Write-Host "Prompt: $prompt" -ForegroundColor Green
 $ResponseMessage = Invoke-ChatCompletion -Prompt $prompt -ApiKey $apiKey -BaseUrl $baseUrl -Model $model
-Write-Output $ResponseMessage
+Write-Output "$($ResponseMessage[0])"
+Write-Host "Response: $($ResponseMessage[1])" -ForegroundColor Yellow
